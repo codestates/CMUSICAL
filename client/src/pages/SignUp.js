edef4d9349c4e40c63a581a9ebea8e4282b51345
yunjink <chloeeekim1109@gmail.com>
2021-11-12 01:11:27 +0900

    Feat: Modify SignUp.js

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,67 +1,122 @@
 import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+// 머지머지머지
 
-const SignUp = () => {
-  const [userId, setUserId] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [cofirmPw, setConfirmPw] = useState('');
+axios.defaults.withCredentials = true;
 
-  const handleUserId = (e) => {
-    setUserId(e.target.value);
+export default function SignUp() {
+  const [userinfo, setuserinfo] = useState({
+    username: '',
+    nickname: '',
+    email: '',
+    password: '',
+  });
+
+  const [confirmPw, setConfirmPw] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  //const navigate = useNavigate();
+
+  const handleInputValue = (key) => (e) => {
+    setuserinfo({ ...userinfo, [key]: e.target.value });
   };
 
-  const handleNickname = (e) => {
-    setNickname(e.target.value);
+  const handleUsernameValidation = () => {
+    const idReg = /^[a-z][a-z0-9]{3,15}$/g;
+    if (!idReg.test(userinfo.username)) {
+      console.log('failed');
+    }
   };
 
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
+  const handleNicknameValidation = () => {
+    const nicknameReg = /^[가-힣a-zA-Z0-9]{2,10}$/g;
+    if (!nicknameReg.test(userinfo.nickname)) {
+      console.log('failed');
+    }
   };
 
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
+  const handleEmailValidation = () => {
+    const emailReg = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (!emailReg.test(userinfo.email)) {
+      console.log('failed');
+    }
+  };
+
+  const handlePasswordValidation = () => {
+    const passwordReg = /^.{8,10}$/g;
+    if (!passwordReg.test(userinfo.password)) {
+      console.log('failed');
+    }
+    if (confirmPw.length > 0) {
+      if (confirmPw === userinfo.password) {
+        console.log('비밀번호가 일치합니다.');
+      } else {
+        console.log('비밀번호가 다릅니다.');
+      }
+    }
   };
 
   const handleConfirmPw = (e) => {
     setConfirmPw(e.target.value);
+    if (userinfo.password !== e.target.value) {
+      console.log('비밀번호가 다릅니다.');
+    } else {
+      console.log('비밀번호가 일치합니다.');
+    }
   };
 
   const handleSignUp = () => {
-    console.log('Sign Up');
+    const { username, nickname, email, password, confirmPassword } = userinfo;
+    if (!username || !nickname || !email || !password || !confirmPassword) {
+      console.log('모든 항목은 필수입니다');
+    }
+    axios.post('https://localhost:4000/signup', { username, nickname, email, password, password }, { headers: { 'Content-Type': 'application/json' } }).then((res) => {
+      // console.log(res.data.message);
+      //navigate('/');
+    });
   };
 
   return (
-    <center>
-      <h1>CMUSICAL</h1>
-      <div>
-        <span>ID</span>
-        <input type="text" value={userId} onChange={handleUserId} />
-        <span>4자 이상 16자 미만 / 중복된 아이디입니다.</span>
-      </div>
-      <div>
-        <span>Nickname</span>
-        <input type="text" value={nickname} onChange={handleNickname} />
-        <span>10자 미만 / 중복된 닉네임입니다.</span>
-      </div>
-      <div>
-        <span>Email</span>
-        <input type="email" value={email} onChange={handleEmail} />
-        <span>이메일 형식을 확인해주세요. / 중복된 이메일입니다. </span>
-      </div>
-      <div>
-        <span>Password</span>
-        <input type="password" value={password} onChange={handlePassword} />
-        <span>8자 이상</span>
-        <span>Confirm Password</span>
-        <input type="password" value={cofirmPw} onChange={handleConfirmPw} />
-        <span>비밀번호가 다릅니다.</span>
-      </div>
-      <button type="button" onClick={handleSignUp}>
-        Sign Up
-      </button>
-    </center>
+    <div>
+      <center>
+        <h1>CMUSICAL</h1>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div>
+            <span>ID</span>
+            <input type="text" onChange={handleInputValue('username')} />
+            <button type="submit" onClick={handleUsernameValidation}>
+              중복 검사
+            </button>
+          </div>
+          <div>
+            <span>Nickname</span>
+            <input type="text" onChange={handleInputValue('nickname')} />
+            <button type="submit" onClick={handleNicknameValidation}>
+              중복 검사
+            </button>
+          </div>
+          <div>
+            <span>Email</span>
+            <input type="email" onChange={handleInputValue('email')} />
+            <button type="submit" onClick={handleEmailValidation}>
+              중복 검사
+            </button>
+          </div>
+          <div>
+            <span>Password</span>
+            <input type="password" onChange={handleInputValue('password')} />
+            <span>오류 메세지</span>
+            <div>
+              <span>Confirm Password</span>
+              <input type="password" value={confirmPw} onChange={handleConfirmPw} />
+              <span>오류 메세지</span>
+            </div>
+          </div>
+          <button type="button" onClick={handleSignUp}>
+            Sign Up
+          </button>
+        </form>
+      </center>
+    </div>
   );
-};
-
-export default SignUp;
+}
