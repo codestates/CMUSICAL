6c107da8231967c3923b5bf94c155f01d81c9413
yunjink <chloeeekim1109@gmail.com>
2021-11-11 14:25:42 +0900

    Feat: Create SignUp Component Modify SignIn.js (변수명 수정)

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 
 const SignIn = () => {
-  const [inputId, setInputId] = useState('');
-  const [inputPw, setInputPw] = useState('');
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleInputId = (e) => {
-    setInputId(e.target.value);
+  const handleUserId = (e) => {
+    setUserId(e.target.value);
   };
 
-  const handleInputPw = (e) => {
-    setInputPw(e.target.value);
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
   };
 
   const handleSignIn = () => {
@@ -20,20 +20,18 @@ const SignIn = () => {
     <div>
       <center>
         <h1>CMUSICAL</h1>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <div>
-            <span>ID</span>
-            <input type="text" value={inputId} onChange={handleInputId} />
-          </div>
-          <div>
-            <span>Password</span>
-            <input type="password" value={inputPw} onChange={handleInputPw} />
-          </div>
-          <button type="submit" onClick={handleSignIn}>
-            Sign In
-          </button>
-          <div>Sign Up</div>
-        </form>
+        <div>
+          <span>ID</span>
+          <input type="text" value={userId} onChange={handleUserId} />
+        </div>
+        <div>
+          <span>Password</span>
+          <input type="password" value={password} onChange={handlePassword} />
+        </div>
+        <button type="submit" onClick={handleSignIn}>
+          Sign In
+        </button>
+        <div>Sign Up</div>
       </center>
     </div>
   );
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [userId, setUserId] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [cofirmPw, setConfirmPw] = useState('');
+
+  const handleUserId = (e) => {
+    setUserId(e.target.value);
+  };
+
+  const handleNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPw = (e) => {
+    setConfirmPw(e.target.value);
+  };
+
+  return (
+    <center>
+      <h1>CMUSICAL</h1>
+      <div>
+        <span>ID</span>
+        <input type="text" value={userId} onChange={handleUserId} />
+        <span>4자 이상 16자 미만 / 중복된 아이디입니다.</span>
+      </div>
+      <div>
+        <span>Nickname</span>
+        <input type="text" value={nickname} onChange={handleNickname} />
+        <span>10자 미만 / 중복된 닉네임입니다.</span>
+      </div>
+      <div>
+        <span>Email</span>
+        <input type="email" value={email} onChange={handleEmail} />
+        <span>이메일 형식을 확인해주세요. / 중복된 이메일입니다. </span>
+      </div>
+      <div>
+        <span>Password</span>
+        <input type="password" value={password} onChange={handlePassword} />
+        <span>8자 이상</span>
+        <span>Confirm Password</span>
+        <input type="password" value={cofirmPw} onChange={handleConfirmPw} />
+        <span>비밀번호가 다릅니다.</span>
+      </div>
+    </center>
+  );
+};
+
+export default SignUp;
