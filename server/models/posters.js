361dd9cc5e7a9cb9c23e06e9818679594db15b1f
hyucki <gurl97@naver.com>
2021-11-15 11:51:55 +0900

    Feat: Create controllers comment

--- server/models/posters.js
+++ server/models/posters.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.posters.belongsTo(models.items, { foreignKey: 'itemId', sourceKey: 'id' });
+      models.posters.belongsTo(models.items, { foreignKey: 'itemId', sourceKey: 'id', onDelete: 'cascade' });
       // define association here
     }
   }
