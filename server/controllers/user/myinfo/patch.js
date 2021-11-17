27d400a9026d5a5b6480a322472864008d758bed
hyucki <gurl97@naver.com>
2021-11-17 22:45:30 +0900

    Feat: Modify cmusical [client] 즐겨찾기 페이지 내에서의 검색과 메인 페이지에서의 검색기능 나눠줌 [server] 정보 수정 시 무조건 비밀번호를 입력해야 하도록 수정

--- server/controllers/user/myinfo/patch.js
+++ server/controllers/user/myinfo/patch.js
@@ -23,22 +23,17 @@ module.exports = {
       // 검증한 토큰의 아이디와 같은 아이디를 가진 유저 정보를 데이터베이스에서 가져온다
       const userInfo = await users.findOne({ where: { id: verifyToken.id }, raw: true });
       const { id, nickname, email, password } = userInfo;
-      // 바꾸고 싶은 비밀번호가 있는데 현재 비밀번호를 입력하지 않았다면
-      if (req.body.newPassword && !req.body.oldPassword) {
-        return res.status(400).send({ message: 'empty oldPassword' });
+      // 비밀번호를 입력하지 않았다면
+      if (!req.body.oldPassword) {
+        return res.status(400).send({ message: 'empty password' });
       }
       // 현재 비밀번호가 입력되었지만 데이터베이스에 있는 비밀번호와 일치하지 않을 경우
       if (!bcrypt.compareSync(req.body.oldPassword, password)) {
         // 유효하지 않는 비밀번호라는 메세지로 응답
         return res.status(400).send({ message: 'invalid password' });
-      } else {
-        // 바꾸고 싶은 비밀번호도 있고 입력한 현재 비밀번호와 데이터베이스의 비밀번호와 일치하고
-        // 변경할 비밀번호가 데이터베이스에 있는 비밀번호와 겹치지 않으면 inspectData에 검사할 비밀번호 추가
-        if (req.body.password !== password) {
-          inspectData.password = req.body.newPassword;
-        }
       }
-
+      // 변경하 비밀번호가 있고 데이터베이스에 있는 비밀번호와 겹치지 않으면 inspectData에 검사할 비밀번호 추가
+      if (req.body.password && req.body.password !== password) inspectData.password = req.body.newPassword;
       if (req.body.nickname !== nickname) inspectData.nickname = req.body.nickname;
       if (req.body.email !== email) inspectData.email = req.body.email;
       // 유효성 검사
@@ -52,7 +47,9 @@ module.exports = {
         return res.status(406).send({ data: { isValid, isConflict } });
       }
       // 유효성 검사와 중복검사를 통과했으면 비밀번호 해싱 후 정보수정 해주기
-      inspectData.password = bcrypt.hashSync(inspectData.password, 10);
+      if (inspectData.password) {
+        inspectData.password = bcrypt.hashSync(inspectData.password, 10);
+      }
       const key = Object.keys(inspectData);
 
       if (key.length === 0) {
