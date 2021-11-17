e80455b0601ad97797d1d34395478d45b20adc25
yunjink <chloeeekim1109@gmail.com>
2021-11-17 23:03:12 +0900

    Feat: Final Modification

--- client/src/components/Posters.js
+++ client/src/components/Posters.js
@@ -1,16 +1,15 @@
 import React from 'react';
 export default function Posters({ posters }) {
-  // TODO: Tab으로부터 받은 받은 상세 이미지 렌더링 -> 상세 이미지가 없을 경우도 예외 처리 해야함
   console.log(posters);
   return (
     <>
       <div>상세 이미지</div>
       <div>
-        {Array.isArray(posters)
+        {posters.length !== 0
           ? posters.map((el, index) => {
               return <img src={el} key={index} width="800px" alt={index}></img>;
             })
-          : ''}
+          : '이미지가 없습니다!'}
       </div>
     </>
   );
--- server/seeders/20211111111857-dummy data.js
+++ server/seeders/20211111111857-dummy data.js
@@ -71,14 +71,7 @@ module.exports = {
           dateFrom: '2021.11.01',
           dateTo: '2021.11.30',
           state: '공연중',
-          poster: {
-            styurl: [
-              'http://www.kopis.or.kr/upload/pfmIntroImage/PF_PF182867_211111_1114043.jpg',
-              'http://www.kopis.or.kr/upload/pfmIntroImage/PF_PF182867_211111_1114042.jpg',
-              'http://www.kopis.or.kr/upload/pfmIntroImage/PF_PF182867_211111_1114041.jpg',
-              'http://www.kopis.or.kr/upload/pfmIntroImage/PF_PF182867_211111_1114040.jpg',
-            ],
-          },
+          poster: null,
           createdAt: '2021-11-13 10:57:35',
           updatedAt: '2021-11-14 10:57:35',
         },
@@ -94,9 +87,7 @@ module.exports = {
           dateFrom: '2021.10.01',
           dateTo: '2021.10.31',
           state: '공연완료',
-          poster: {
-            styurl: 'http://www.kopis.or.kr/upload/pfmIntroImage/PF_PF182867_211111_1114043.jpg',
-          },
+          poster: null,
           createdAt: '2021-11-13 10:57:35',
           updatedAt: '2021-11-14 10:57:35',
         },
