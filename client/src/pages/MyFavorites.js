b781394732c749968d27c6027ae3b28f9973dff2
shleecloud <shleecloud@naver.com>
2021-11-17 16:36:50 +0900

    Feat: Favorite Page Complete

--- client/src/pages/MyFavorites.js
+++ client/src/pages/MyFavorites.js
@@ -1,11 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from '../components/Navigation';
-import Thumbnail from '../components/Thumbnail';
 import Footer from '../components/Footer';
+import Thumbnail from '../components/Thumbnail';
+import getAuth from '../functions/getAuth';
 import styled from 'styled-components';
 import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
+
+axios.defaults.withCredentials = true;
+
+export const Body = styled.div`
+  width: auto;
+  min-height: auto;
+  border: 3px solid green;
+
+  > .list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    border: 3px solid green;
+    margin: 10px 50px;
+    padding: 10px;
+  }
+`;
 
 export const Container = styled.div`
   width: auto;
@@ -18,21 +36,58 @@ export const Container = styled.div`
   }
 `;
 
-
+// export default function MyFavorites({ isLogin, loginHandler, logoutHandler }) {
 export default function MyFavorites({ isLogin, loginHandler, logoutHandler }) {
-  // TODO: useEffect() - GET 요청으로 즐겨찾기 목록 조회하기
+  const [list, setList] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  const handleFilter = async text => {
+    // let totalList;
+    // if (text) {
+    //   totalList = await axios.get(`${process.env.REACT_APP_SERVER_ADDR}?title=${text}`);
+    // } else {
+    //   totalList = await axios.get(`${process.env.REACT_APP_SERVER_ADDR}`);
+    //   // console.log(isLogin);
+    // }
+    // let favoritesList;
+    // const getLogin = getAuth(loginHandler, logoutHandler);
+    // if (getLogin) {
+    //   favoritesList = await axios.get(`${process.env.REACT_APP_SERVER_ADDR}/favorites`);
+    //   setFavorites(favoritesList.data.items);
+    //   setList(favoritesList.data.items);
+    // }
+
+    let favoritesList;
+    if (text) {
+      favoritesList = await axios.get(`${process.env.REACT_APP_SERVER_ADDR}/favorites?title=${text}`);
+    } else {
+      favoritesList = await axios.get(`${process.env.REACT_APP_SERVER_ADDR}/favorites`);
+    }
+    await getAuth(loginHandler, logoutHandler);
+    setFavorites(favoritesList.data.items);
+    setList(favoritesList.data.items);
+  };
+
+  useEffect(() => {
+    handleFilter(window.sessionStorage.getItem('Keyword'));
+  }, []);
 
   return (
-    <Container>
-      <Navigation isLogin={isLogin} loginHandler={loginHandler} logoutHandler={logoutHandler} />
-      <div id="body">
-        {/* {favoList.length > 0
-          ? favoList.map(el => {
-              return <Thumbnail key={el.id} poster={el.thumbnail} title={el.title} id={el.id} />;
-            })
-          : '즐겨찾기를 추가해보세요!'} */}
-      </div>
+    <>
+      <Navigation handleFilter={handleFilter} isLogin={isLogin} loginHandler={loginHandler} logoutHandler={logoutHandler} />
+      <Body>
+        <div className="title">
+          <h2>Favorites</h2>
+        </div>
+        <div className="list">
+          {Array.isArray(list)
+            ? list.map((el, idx) => {
+                return <Thumbnail isLogin={isLogin} key={idx} thumbnail={el.thumbnail} title={el.title} id={el.id} favorites={favorites} setFavorites={setFavorites} />;
+              })
+            : '로딩 이미지'}
+        </div>
+      </Body>
       <Footer />
-    </Container>
+    </>
   );
 }
