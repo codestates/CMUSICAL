d7367730bc1c76edd82091028b851807c4e21c41
EUNYUSEO <eyseo328@gmail.com>
2021-11-17 17:45:32 +0900

    Change branch

--- client/src/components/Posters.js
+++ client/src/components/Posters.js
@@ -1,6 +1,5 @@
 import React from 'react';
 export default function Posters({ posters }) {
-  // TODO: Tab으로부터 받은 받은 상세 이미지 렌더링 -> 상세 이미지가 없을 경우도 예외 처리 해야함
   console.log(posters);
   return (
     <>
