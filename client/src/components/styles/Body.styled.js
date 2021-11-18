8965676359d05398fd75146ad7c24e6d22a81a96
shleecloud <shleecloud@gmail.com>
2021-11-18 14:42:35 +0900

    Update Body.styled.js

--- client/src/components/styles/Body.styled.js
+++ client/src/components/styles/Body.styled.js
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const Body = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 0 30vh 12rem; 30vh;
+  padding: 0 30vh 12rem 30vh;
 `;
